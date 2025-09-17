9bbf16cd2f761b742304e05a71f72c52f46d4697
icutter <damiano.tagliaferri@proton.me>
2025-09-17 19:08:41 +0200

    chore: update cron triggers

--- src/cron-triggers.ts
+++ src/cron-triggers.ts
@@ -1,3 +1,3 @@
 // IMPORTANT: Remember to update wrangler.toml with the same cron expressions
-export const CRON_LEVEL_UP_EVENTS = "0 15 * * *";  // Every day at 15:00 (UTC)
-export const CRON_FREE_GAMES = "0 17 * * 5";  // Every Thursday at 17:00 (UTC)
+export const CRON_LEVEL_UP_EVENTS = "20 15 * * *";  // Every day at 15:20 (UTC)
+export const CRON_FREE_GAMES = "20 15 * * 5";  // Every Thursday at 15:20 (UTC)
